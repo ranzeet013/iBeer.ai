2ab7f1623b803f402e7b0de7e5540e6b9782b1bd
ranzeet013 <ranzeetraut00@gmail.com>
2025-02-01 19:52:36 +0545

    created config.py

---
+++ beerLabelClassification/classificationAPI/config.py
@@ -0,0 +1,4 @@
+import os
+
+MODEL_PATH = 'C:/Users/suj33/Desktop/iBeer.ai/beerLabelClassification/weights/label_classifiations.pth'
+CLASSIFIED_LABELS_PATH = r'C:/Users/suj33/Desktop/iBeer.ai/beerLabelClassification/ClassificationAPI/labelClassified'
